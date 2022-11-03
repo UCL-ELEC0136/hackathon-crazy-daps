08efbd962ce3f7d02a5028dcbe3aef0eca355a58
Yang Li <66520315+Yang-Li86@users.noreply.github.com>
2022-11-03 16:00:38 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import requests
 import pymongo
 
-server = pymongo.MongoClient("mongodb+srv://student:assignment@cluster0.iqqk4u7.mongodb.net/?retryWrites=true&w=majority")
+
+client = pymongo.MongoClient("mongodb+srv://student:assignment@cluster0.iqqk4u7.mongodb.net/?retryWrites=true&w=majority")
+db = client.daps_hackathon
+
+# # Upload to MangoDB
+collection = db.daps_hackathon
+
+
+repo_per_page = []
 page = 1
+
 while True:
-    r_google = requests.get("https://api.github.com/orgs/google/repos?per_page=100&page={}".format(page))
-    repo = r_google.json()
-    if len(repo) == 0:
+    url = "https://api.github.com/orgs/Google/repos?per_page=100&page={}".format(page)
+    response = requests.get(url, auth=('Deng-1-Pan', "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"))
+    
+    repos = response.json()
+    if len(repos) == 0:
         break
-
-    database = server["hackathon"]
-    collection = database["repo"]
-    collection.insert_many(repo)
-
+    
+    # repo_per_page.append(repos)
+    
+    upload1 = collection.insert_many(repos)
+    
     page += 1
-
+# client = pymongo.MongoClient("mongodb+srv://student:assignment@cluster0.iqqk4u7.mongodb.net/?retryWrites=true&w=majority")
+# db= client.admin
 
 
