bcfc9dc989999602ab8ca84c220f6d9a27a37767
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-11-03 14:30:15 +0000

    client added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import pymongo
 
 r = requests.get('https://api.github.com/orgs/Google/repos')
 print(r.text)
+
+client = pymongo.MongoClient("mongodb+srv://student:assignment@cluster0.iqqk4u7.mongodb.net/?retryWrites=true&w=majority")
